61e73238ccc682e8496164685ef2abc7169dcb0f
faselbaum <juliuswalther1991@googlemail.com>
2024-09-19 15:26:30 +0200

    wip

--- apps/examples-react/stories/react/Advanced Component Modification/intro.stories.tsx
+++ apps/examples-react/stories/react/Advanced Component Modification/intro.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryFn } from '@storybook/react'
 
 import * as CardApplicationPlain from '@veavr/react-components/components/card/application.plain.js'
 
-import CardApplicationPlainSourceCode from '../../../../../packages/veavr-react-components/src/components/card/application.plain.tsx?raw'
+import ComponentSourceCode from '../../../../../packages/veavr-react-components/src/components/card/component.tsx?raw'
 
 export default {} satisfies Meta
 
@@ -10,7 +10,7 @@ export const CardStory: StoryFn = () => <CardApplicationPlain.Application />
 CardStory.parameters = {
   docs: {
     source: {
-      code: CardApplicationPlainSourceCode,
+      code: ComponentSourceCode,
     },
   },
 }
